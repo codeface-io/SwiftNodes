bbcc927714e4523760747ff0e95780e1f16956d9
Sebastian Fichtner <contact@flowtoolz.com>
2022-10-10 22:14:20 +0200

    Stabilize dependency version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/flowtoolz/SwiftyToolz.git",
-            .branch("master")
+            exact: "0.1.0"
         ),
         .package(
             url: "https://github.com/apple/swift-collections.git",
