f2d715e9e91c4d55c2240b7ed240eb86a66cb9a8
Sebastian Fichtner <contact@flowtoolz.com>
2023-01-10 15:37:26 +0100

    Add test for sub graph

--- Tests/SwiftNodesTests.swift
+++ Tests/SwiftNodesTests.swift
@@ -179,5 +179,31 @@ class SwiftNodesTests: XCTestCase {
         XCTAssertNil(meg.edge(from: "id1", to: "id3"))
     }
     
+    func testSubGraph() {
+        var graph = Graph<Int, Int>()
+        
+        for value in 0 ... 20
+        {
+            graph.insert(value)
+        }
+        
+        for value in 0 ... 19
+        {
+            graph.addEdge(from: value, to: value + 1)
+        }
+        
+        XCTAssertEqual(graph.edges.count, 20)
+        XCTAssertNotNil(graph.edge(from: 12, to: 13))
+        
+        let subsetOfNodeIDs: Set<Int> = [0, 3, 6, 9, 12, 13, 14, 15]
+        let subGraph = graph.subGraph(nodeIDs: subsetOfNodeIDs)
+        
+        XCTAssertEqual(graph.edges.count, 20)
+        XCTAssertNotNil(graph.edge(from: 9, to: 10))
+        
+        XCTAssertEqual(subGraph.edges.count, 3)
+        XCTAssertNil(subGraph.edge(from: 9, to: 10))
+    }
+    
     // TODO: Test more algorithms
 }
