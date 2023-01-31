65d802e730687e4a0d3fdab8b27424c98de4165d
Sebastian Fichtner <contact@flowtoolz.com>
2023-01-31 13:38:20 +0100

    Use condensation graph Equatability in tests

--- Tests/Algorithms/CondensationGraphTests.swift
+++ Tests/Algorithms/CondensationGraphTests.swift
@@ -5,15 +5,39 @@ class CondensationGraphTests: XCTestCase {
     
     func testGraphWithoutEdges() {
         let graph = Graph(values: [1, 2, 3])
+        
         let condensationGraph = graph.makeCondensationGraph()
-        XCTAssertEqual(condensationGraph.nodes.count, 3)
+        
+        let expectedCondensationGraph = Graph<Int, Int>.CondensationGraph(
+            values: [
+                .init(nodeIDs: [1]),
+                .init(nodeIDs: [2]),
+                .init(nodeIDs: [3])
+            ]
+        )
+        
+        XCTAssertEqual(condensationGraph, expectedCondensationGraph)
     }
     
     func testGraphWithoutCycles() {
         let graph = Graph(values: [1, 2, 3, 4],
-        edges: [(1, 2), (2, 3), (3, 4), (1, 3), (2, 4)])
+                          edges: [(1, 2), (2, 3), (3, 4), (1, 3), (2, 4)])
+        
         let condensationGraph = graph.makeCondensationGraph()
-        XCTAssertEqual(condensationGraph.nodes.count, 4)
+        
+        let expectedCondensationGraph = Graph<Int, Int>.CondensationGraph(
+            values: [
+                .init(nodeIDs: [1]),
+                .init(nodeIDs: [2]),
+                .init(nodeIDs: [3]),
+                .init(nodeIDs: [4])
+            ],
+            edges: [
+                ([1], [2]), ([2], [3]), ([3], [4]), ([1], [3]), ([2], [4])
+            ]
+        )
+        
+        XCTAssertEqual(condensationGraph, expectedCondensationGraph)
     }
     
     func testGraphMadeOfOneCycle() {
@@ -21,11 +45,12 @@ class CondensationGraphTests: XCTestCase {
                           edges: [(1, 2), (2, 3), (3, 4), (4, 1)])
         
         let condensationGraph = graph.makeCondensationGraph()
-        XCTAssertEqual(condensationGraph.nodes.count, 1)
-        XCTAssertEqual(condensationGraph.edges.count, 0)
         
-        let condensationNodeSizes = condensationGraph.values.map { $0.nodes.count }.sorted()
-        XCTAssertEqual(condensationNodeSizes, [4])
+        let expectedCondensationGraph = Graph<Int, Int>.CondensationGraph(
+            values: [.init(nodeIDs: [1, 2, 3, 4])]
+        )
+        
+        XCTAssertEqual(condensationGraph, expectedCondensationGraph)
     }
     
     func testGraphContainingOneCycle() {
@@ -33,11 +58,13 @@ class CondensationGraphTests: XCTestCase {
                           edges: [(1, 2), (2, 3), (3, 1), (3, 4)])
         
         let condensationGraph = graph.makeCondensationGraph()
-        XCTAssertEqual(condensationGraph.nodes.count, 2)
-        XCTAssertEqual(condensationGraph.edges.count, 1)
         
-        let condensationNodeSizes = condensationGraph.values.map { $0.nodes.count }.sorted()
-        XCTAssertEqual(condensationNodeSizes, [1, 3])
+        let expectedCondensationGraph = Graph<Int, Int>.CondensationGraph(
+            values: [.init(nodeIDs: [1, 2, 3]), .init(nodeIDs: [4])],
+            edges: [([1, 2, 3], [4])]
+        )
+        
+        XCTAssertEqual(condensationGraph, expectedCondensationGraph)
     }
     
     func testGraphContainingTwoCycles() {
@@ -45,10 +72,12 @@ class CondensationGraphTests: XCTestCase {
                           edges: [(1, 2), (2, 3), (3, 1), (3, 4), (4, 5), (5, 6), (6, 4)])
         
         let condensationGraph = graph.makeCondensationGraph()
-        XCTAssertEqual(condensationGraph.nodes.count, 2)
-        XCTAssertEqual(condensationGraph.edges.count, 1)
         
-        let condensationNodeSizes = condensationGraph.values.map { $0.nodes.count }.sorted()
-        XCTAssertEqual(condensationNodeSizes, [3, 3])
+        let expectedCondensationGraph = Graph<Int, Int>.CondensationGraph(
+            values: [.init(nodeIDs: [1, 2, 3]), .init(nodeIDs: [4, 5, 6])],
+            edges: [([1, 2, 3], [4, 5, 6])]
+        )
+        
+        XCTAssertEqual(condensationGraph, expectedCondensationGraph)
     }
 }
