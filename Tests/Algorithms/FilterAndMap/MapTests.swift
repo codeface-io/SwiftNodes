a60b7c348a392ac3ebaddfd9d26590e4e580b48b
Sebastian Fichtner <contact@flowtoolz.com>
2023-02-13 09:43:47 +0100

    Edit tests

--- Tests/Algorithms/FilterAndMap/MapTests.swift
+++ Tests/Algorithms/FilterAndMap/MapTests.swift
@@ -4,8 +4,8 @@ import XCTest
 class MapTests: XCTestCase {
         
     func testMappingIntValuesToString() {
-        let intGraph: Graph<Int, Int, Int> = [1, 2, 3]
-        let stringGraph = intGraph.map { "\($0)" }
+        let graph: TestGraph = [1, 2, 3]
+        let stringGraph = graph.map { "\($0)" }
         let expectedStringGraph: Graph<Int, String, Int> = [1: "1", 2: "2", 3: "3"]
         XCTAssertEqual(stringGraph, expectedStringGraph)
     }
--- Tests/Algorithms/FilterAndMap/ValueFilterTests.swift
+++ Tests/Algorithms/FilterAndMap/ValueFilterTests.swift
@@ -4,7 +4,7 @@ import XCTest
 class ValueFilterTests: XCTestCase {
     
     func testValueFilter() {
-        let graph: Graph<Int, Int, Int> = [1, 2, 10, 20]
+        let graph: TestGraph = [1, 2, 10, 20]
         let filteredGraph = graph.filtered { $0 < 10 }
         let expectedFilteredGraph: Graph<Int, Int, Int> = [1, 2]
         XCTAssertEqual(filteredGraph, expectedFilteredGraph)
